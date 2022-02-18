b807f200d3c66d934891cab0287942d5e6eb6699
Tim <tja2353@gmail.com>
2022-02-18 17:07:39 +0000

    Add jest

---
+++ angularfire-app/jest.config.js
@@ -0,0 +1,8 @@
+/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
+module.exports = {
+  preset: 'jest-preset-angular',
+  moduleNameMapper: {
+    '^@app1/services/(.*)$': '<rootDir>/src/app/services/$1',
+  },
+  setupFilesAfterEnv: ['<rootDir>/setup-jest.ts'],
+};
---
+++ jest.config.js
@@ -0,0 +1,10 @@
+/** @type {import('ts-jest/dist/types').InitialOptionsTsJest} */
+module.exports = {
+  // globalSetup: 'jest-preset-angular/global-setup',
+  projects: [
+    "<rootDir>/angularfire-app",
+    "<rootDir>/angularfire-compat-app",
+    "<rootDir>/firebase-app",
+    "<rootDir>/firebase-compat-app",
+  ],
+};
